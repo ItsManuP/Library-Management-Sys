f3909b5319fa1465edb0937aac41efd7ce0f0520
ItsManuP <manuel140298@live.it>
2024-03-20 16:32:16 +0100

    first commit

---
+++ SistemaBibliotecario/src/AggiungiLibro.java
@@ -0,0 +1,78 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileReader;
+
+public class AggiungiLibro {
+    
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Inserisci il titolo del libro: ");
+        String titolo = scanner.nextLine();
+
+        System.out.println("Inserisci l'autore del libro: ");
+        String autore = scanner.nextLine();
+
+        System.out.println("Inserisci il genere del libro: ");
+        String genere = scanner.nextLine();
+
+        System.out.println("Inserisci l'editore del libro: ");
+        String editore = scanner.nextLine();
+
+        System.out.println("Inserisci l'anno di pubblicazione del libro: ");
+        int annoPubblicazione = scanner.nextInt();
+
+        System.out.println("Inserisci l'ISBN del libro: ");
+        String isbn = scanner.nextLine();
+
+        System.out.println("Inserisci la descrizione del libro: ");
+        String descrizione = scanner.nextLine();
+
+        System.out.println("Inserisci la disponibilità del libro: ");
+        String disponibilità = scanner.nextLine();
+
+        // Creo oggetto Libro
+        Libro libro = new Libro(titolo, autore, genere, editore, annoPubblicazione, isbn, descrizione, disponibilità);
+
+        // Prendo l'isbn come codice identificativo, suppongo ogni isbn sia univoco.
+        String json = "{" +
+        "\"titolo\":\"" + libro.getTitolo() + "\"," +
+        "\"autore\":\"" + libro.getAutore() + "\"," +
+        "\"genere\":\"" + libro.getGenere() + "\"," +
+        "\"editore\":\"" + libro.getEditore() + "\"," +
+        "\"annoPubblicazione\":" + libro.getAnnoPubblicazione() + "," +
+        "\"isbn\":\"" + libro.getIsbn() + "\"," +
+        "\"descrizione\":\"" + libro.getDescrizione() + "\"," +
+        "\"disponibilita\":" + libro.isDisponibilità() +
+    "}";
+    
+    
+
+
+     // Leggi contenuto del file (se esiste)
+     String oldContent = "";
+     try (FileReader fileReader = new FileReader("SistemaBibliotecario/src/dati/libri.json")) {
+       int c;
+       StringBuilder stringBuilder = new StringBuilder();
+       while ((c = fileReader.read()) != -1) {
+         stringBuilder.append((char) c);
+       }
+       oldContent = stringBuilder.toString();
+     } catch (IOException e) {
+       // File non esiste, crea un nuovo file vuoto(In caso TODO)
+     }
+ 
+     // Concateno i json
+     String concatenatedJson = oldContent.isEmpty() ? json : oldContent.substring(0, oldContent.length() - 1) + "}"+ "," + "\n" + json ;
+ 
+     // Scrivi contenuto concatenato sul file
+     try (FileWriter fileWriter = new FileWriter("SistemaBibliotecario/src/dati/libri.json")) {
+       fileWriter.write(concatenatedJson);
+     } catch (IOException e) {
+       e.printStackTrace();
+     }
+   }
+ }
+
+
---
+++ SistemaBibliotecario/src/Libro.java
@@ -0,0 +1,105 @@
+
+
+
+
+
+public class Libro {
+
+    private String titolo;
+    private String autore;
+    private String genere;
+    private String editore;
+    private int annoPubblicazione;
+    private String isbn;
+    private String descrizione;
+    private String disponibilità;
+    
+    public Libro(String titolo, String autore, String genere, String editore, int annoPubblicazione, String isbn, String descrizione, String disponibilità) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.genere = genere;
+        this.editore = editore;
+        this.annoPubblicazione = annoPubblicazione;
+        this.isbn = isbn;
+        this.descrizione = descrizione; 
+        this.disponibilità = disponibilità;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    } 
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    public String getEditore() {
+        return editore;
+    }
+
+    public void setEditore(String editore) {
+        this.editore = editore;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String isDisponibilità() {
+        return disponibilità;
+    }
+    
+    public void setDisponibilita(String disponibilità) {
+        this.disponibilità = disponibilità;
+    }
+
+    public String getInformazioniLibro(){
+        return
+        "Titolo: " + titolo + "\n"
+        + "\nAutore: " + autore + "\n"
+        + "\nGenere: " + genere + "\n"
+        + "\nEditore: " + editore + "\n"
+        + "\nAnno di pubblicazione: " + annoPubblicazione + "\n"
+        + "\nISBN: " + isbn + "\n"
+        + "\nDescrizione: " + descrizione + "\n"
+        + "\nDisponibilità: " + disponibilità ;
+        
+    }
+
+}
