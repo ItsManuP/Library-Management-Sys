dfae90a061014af4591a052cfe7d4abb0377447b
ItsManuP <manuel140298@live.it>
2024-03-23 16:13:53 +0100

    -WIP json to list to delete a specific book by book_title.

--- SistemaBibliotecario/src/EliminaLibro.java
+++ SistemaBibliotecario/src/EliminaLibro.java
@@ -1,25 +1,66 @@
 import com.google.gson.Gson;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.FileWriter;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class EliminaLibro {
-    
-    public static void main(String[] args){
-    try{
+
+    public static void main(String[] args) throws IOException {
         String filepath = "SistemaBibliotecario/src/dati/libri.json";
-        Gson gson = new Gson();
-        FileReader fileReader = new FileReader(filepath);
-        Libro[] libri = gson.fromJson(fileReader, Libro[].class);
-        for(Libro libro: libri){
-            System.out.println("Titolo: "+ libro.getTitolo());
+
+        // Load books from JSON
+        List<Libro> libri = loadBooks(filepath);
+        System.out.println(libri);
+        for(Libro libro : libri) {
+            System.out.println("Titolo del libro: " + libro.getTitolo());
         }
-    } catch (IOException e) {
-        e.printStackTrace();
-      }
 
+        // Get user input for book title to delete
+        System.out.println("Inserisci il titolo del libro che vorresti eliminare: ");
+        Scanner scanner = new Scanner(System.in);
+        String titleToDelete = scanner.nextLine();
+        scanner.close();
 
+        // Find and delete book
+        int index = findBookIndex(titleToDelete, libri);
+        if (index != -1) {
+            libri.remove(index);
+            saveBooks(libri, filepath);
+            System.out.println("Libro eliminato correttamente!");
+        } else {
+            System.out.println("Libro non trovato.");
+        }
+    }
 
-    
+    // Load books from JSON file
+    private static List<Libro> loadBooks(String filepath) throws IOException {
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<Libro>>() {}.getType();
+        try (FileReader reader = new FileReader(filepath)) {
+            return gson.fromJson(reader, listType);
+        }
+    }
 
+    // Find index of book with matching title
+    private static int findBookIndex(String titleToDelete, List<Libro> libri) {
+        for (int i = 0; i < libri.size(); i++) {
+            if (libri.get(i).getTitolo().equals(titleToDelete)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Save list of books to JSON file
+    private static void saveBooks(List<Libro> libri, String filepath) throws IOException {
+        Gson gson = new Gson();
+        String json = gson.toJson(libri);
+        try (FileWriter writer = new FileWriter(filepath)) {
+            writer.write(json);
+        }
     }
 }
